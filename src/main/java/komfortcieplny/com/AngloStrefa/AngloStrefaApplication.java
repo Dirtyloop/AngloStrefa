d94bd211db5f140bf6af156ec5800d092cd8ee39
Michal Nowakowski <michal.nowakowski85@gmail.com>
2023-03-01 21:55:11 +0100

    initial commit

---
+++ src/main/java/komfortcieplny/com/AngloStrefa/AngloStrefaApplication.java
@@ -0,0 +1,13 @@
+package komfortcieplny.com.AngloStrefa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngloStrefaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngloStrefaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/komfortcieplny/com/AngloStrefa/AngloStrefaApplicationTests.java
@@ -0,0 +1,13 @@
+package komfortcieplny.com.AngloStrefa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngloStrefaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
